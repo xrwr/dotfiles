d79d6bb672d670a4cc75b6ff60839d91fd527319
st-8 <sasaki.r.ap@m.titech.ac.jp>
2022-06-12 17:39:08 +0900

    fix

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -20,3 +20,14 @@ elif [[ $ARCH == x86_64 ]]; then
     echo "Current Architecture: $ARCH"
 	eval $(/usr/local/bin/brew shellenv)
 fi
+
+# cdr, add-zsh-hook を有効にする
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+
+# cdr の設定
+zstyle ':completion:*' recent-dirs-insert both
+zstyle ':chpwd:*' recent-dirs-max 500
+zstyle ':chpwd:*' recent-dirs-default true
+zstyle ':chpwd:*' recent-dirs-file "$HOME/.cache/shell/chpwd-recent-dirs"
+zstyle ':chpwd:*' recent-dirs-pushd true
