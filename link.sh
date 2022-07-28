c9a78a2a9d178d5aaa16dd6008662330c1526b8d
refv-mac-mini <sasaki.r.ap@m.titech.ac.jp>
2022-07-28 16:26:16 +0900

    rename files and delete .gitconfig

--- link.sh
+++ link.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # 配置したい設定ファイル
-dotfiles=(zshrc latexmkrc vimrc gitconfig condarc)
+dotfiles=(.zshrc .latexmkrc .vimrc .condarc)
 
 # 設定ファイルのシンボリックリンクを
 # ホームディレクトリ直下に作成する
 for file in "${dotfiles[@]}"; do
-        ln -svf ~/dotfiles/$file ~/.$file
+        ln -svf ~/dotfiles/$file ~/$file
 done
