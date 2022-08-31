2d6ccb097f71983bf93febb6831a74fe0677052c
refv-mac-mini <sasaki.r.ap@m.titech.ac.jp>
2022-08-31 19:14:43 +0900

    add nvm settings

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -7,3 +7,9 @@ export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 # ---
+
+# --- nvm settings
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+# ---
